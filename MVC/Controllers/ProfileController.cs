5d6a244d0a9b8ed7df14ea84eaf0f2fd45030e56
ridhoandhika <ridhoandhika74@gmail.com>
2019-08-15 14:06:03 +0700

    first

---
+++ MVC/Controllers/ProfileController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public static List<Models.Profile> ListProfile = new List<Models.Profile>();
+        public ActionResult Index()
+        {
+            return View(ListProfile);
+        }
+        // GET: Profile
+        public ActionResult AddProfile()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProfile(Models.Profile model)
+        {
+            ListProfile.Add(model);
+            return View("Detail", model);
+            
+        }
+        
+        public ActionResult Detail()
+        {
+            Models.Profile item = new Models.Profile()
+            {
+                ID = 1,
+                NamaLengkap = "Ahmad Roni",
+                Alamat = "Jakarta",
+                Hobi = "hiking"
+            };
+            return View(item);
+        }
+    }
+}
---
+++ MVC/Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Profile
+    {
+        public int ID { get; set; }
+        public string NamaLengkap { get; set; }
+        public string Alamat { get; set; }
+        public string Hobi { get; set; }
+    }
+}
